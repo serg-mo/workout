da88cbc0c03842a11b75f22eb54a17858ae41535
Sergey Morozov <morozov.sergey@gmail.com>
2024-05-03 09:55:50 -0700

    init/reset/export buttons

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { eraseLocalStorage, getLocalStorage, setLocalStorage } from "./lib";
 
 export default function Footer() {
-  const onInit = () => {
+  const [exportString, setExportString] = useState("");
+
+  const onImport = () => {
     const payload = JSON.parse(prompt("JSON")); // NOTE: must be in double quotes
 
     if (Object.keys(payload).length) {
@@ -21,36 +23,42 @@ export default function Footer() {
     return eraseLocalStorage();
   };
   const onExport = () => {
-    if (!window.confirm("Are you sure?")) {
-      return;
-    }
     // NOTE: copy to clipboard does not work on mobile
-    // TODO: build a textarea and copy the content
-    console.log(getLocalStorage());
+    setExportString(JSON.stringify(getLocalStorage()));
   };
 
   return (
-    <div className="w-full flex flex-row justify-center mt-3">
-      <button
-        className="m-auto bg-blue-200 p-2 font-bold rounded text-sm"
-        onClick={onInit}
-      >
-        Init
-      </button>
+    <>
+      <div className="w-full flex flex-row justify-center mt-3">
+        <button
+          className="m-auto bg-blue-200 p-2 font-bold rounded text-sm"
+          onClick={onImport}
+        >
+          Import
+        </button>
 
-      <button
-        className="m-auto bg-blue-200 p-2 font-bold rounded text-sm"
-        onClick={onReset}
-      >
-        Reset
-      </button>
+        <button
+          className="m-auto bg-blue-200 p-2 font-bold rounded text-sm"
+          onClick={onReset}
+        >
+          Reset
+        </button>
 
-      <button
-        className="m-auto bg-blue-200 p-2 font-bold rounded text-sm"
-        onClick={onReset}
-      >
-        Export
-      </button>
-    </div>
+        <button
+          className="m-auto bg-blue-200 p-2 font-bold rounded text-sm"
+          onClick={onExport}
+        >
+          Export
+        </button>
+      </div>
+      {exportString && (
+        <textarea
+          value={exportString}
+          className="w-full text-xs mt-5 p-0 text-wrap"
+          rows={5}
+          onChange={() => {}}
+        />
+      )}
+    </>
   );
 }
