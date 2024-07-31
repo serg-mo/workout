abaa4eb8233b494713b273bbd04b6eafc6649a7e
Sergey Morozov <morozov.sergey@gmail.com>
2024-07-31 15:09:14 -0700

    my workout weight options

--- src/sergey.js
+++ src/sergey.js
@@ -1,7 +1,7 @@
 // kettlebells: 8.8, 13.2, 17.6, 22, 26.4, 30.8, 35.2, 39.6, 44, 48.4, 52.8,
 const LEGS = {
   // treadmill 2min each: forward front/back, sideways left/right, backwards
-  Squat: [135, 145, 155, 165, 175, 185], // quads, hams, glutes
+  Squat: [135, 155, 175], // quads, hams, glutes
   "Kettlebell Lunges": [
     8.8, 13.2, 17.6, 22, 26.4, 30.8, 35.2, 39.6, 44, 48.4, 52.8,
   ], // glutes, hams, quads, calves
@@ -9,17 +9,14 @@ const LEGS = {
 };
 
 const FRONT = {
-  "Bench Press": [135, 145, 155, 165, 175, 185], // pecs, delts, triceps, biceps, serratus
+  "Bench Press": [135, 155, 175], // pecs, delts, triceps, biceps, serratus
   "Overhead Press": [45, 55, 65, 75, 85, 95, 105, 115, 125, 135], // pecs, delts, triceps, traps
   "Bicep Curls": [20, 25, 30, 35, 40, 45], // biceps
   Abs: [200], // (bodyweight)
 };
 
 const BACK = {
-  Deadlift: [
-    135, 145, 155, 165, 175, 185, 195, 205, 215, 225, 235, 245, 255, 265, 275,
-    285,
-  ], // glutes, hams, core, back, traps
+  Deadlift: [135, 155, 175, 195, 225, 245, 265, 285], // glutes, hams, core, back, traps
   "Cable Rows": [80, 90, 100, 110, 120, 130, 140, 150], // lats, traps, delts, bicpes, triceps
   "Cable Pull Downs": [100, 110, 120, 130, 140], // lats, traps, biceps, core, delts, shoulders
   "Cable Tricep": [20, 30, 40, 50], // triceps
