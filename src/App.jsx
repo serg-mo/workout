ae4c58df6dbcf6628b25ec57965bf135b163d980
Sergey Morozov <morozov.sergey@gmail.com>
2025-09-19 19:57:44 -0400

    proper weight options for my equipment

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ import Setup from './components/Setup';
 import Workouts from './components/Workouts';
 import { formatDate, formatHistory, formatSet, getLocalStorage, setLocalStorage } from './lib';
 
+// TODO: I can see my github build on my Apple watch.state works, but the zoom is weird
+// TODO: I've got to make this  look good on a Apple Watch screen, right now only iPhone
+// TODO: save and load state with QR codes
+// TODO: since state lives on Apple Watch, it needs a way to backup state in a QR code
+// TODO: I'm sure there is a way to do screenshots in Apple Watch, right
+// TODO: confirmed that a screenshot of a QR code made on Apple Watch can be seen and interpreted by an iPhone
+
 // NOTE: no need to ever refresh the page, unless to pick a different workout
 export default function App() {
   const [workoutName, setWorkoutName] = useState('');
